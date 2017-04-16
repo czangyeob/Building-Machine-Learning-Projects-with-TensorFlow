b69ed6c109cd193b012889c7ca280247a83cb330
Matthew <czangyeob@gmail.com>
2017-04-16 21:35:30 +0900

    RNN Code PN to Py

--- 7/Code/CH7_time_series.py
+++ 7/Code/CH7_time_series.py
@@ -7,50 +7,17 @@ from matplotlib import pyplot as plt
 from tensorflow.python.framework import dtypes
 from tensorflow.contrib import learn
 
-import logging
-logging.basicConfig(level=logging.INFO)
-
-
-from tensorflow.contrib import learn
-from sklearn.metrics import mean_squared_error
-
-LOG_DIR = './ops_logs'
-TIMESTEPS = 5
-RNN_LAYERS = [{'steps': TIMESTEPS}]
-DENSE_LAYERS = None
-TRAINING_STEPS = 10000
-BATCH_SIZE = 100
-PRINT_STEPS = TRAINING_STEPS / 100
-
-def lstm_model(time_steps, rnn_layers, dense_layers=None):
-    def lstm_cells(layers):
-        return [tf.nn.rnn_cell.BasicLSTMCell(layer['steps'],state_is_tuple=True)
-                for layer in layers]
-
-    def dnn_layers(input_layers, layers):
-            return input_layers
-
-    def _lstm_model(X, y):
-        stacked_lstm = tf.nn.rnn_cell.MultiRNNCell(lstm_cells(rnn_layers), state_is_tuple=True)
-        x_ = learn.ops.split_squeeze(1, time_steps, X)
-        output, layers = tf.nn.rnn(stacked_lstm, x_, dtype=dtypes.float32)
-        output = dnn_layers(output[-1], dense_layers)
-        return learn.models.linear_regression(output, y)
-
-    return _lstm_model
-
-
-regressor = learn.TensorFlowEstimator(model_fn=lstm_model(TIMESTEPS, RNN_LAYERS, DENSE_LAYERS), n_classes=0,
-                                      verbose=2,  steps=TRAINING_STEPS, optimizer='Adagrad',
-                                      learning_rate=0.03, batch_size=BATCH_SIZE)
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
+from keras.layers.recurrent import LSTM
+from keras.layers import Dropout
 
 df = pd.read_csv("data/elec_load.csv", error_bad_lines=False)
 plt.subplot()
 plot_test, = plt.plot(df.values[:1500], label='Load')
 plt.legend(handles=[plot_test])
 
-
-print df.describe()
+print(df.describe())
 array=(df.values- 147.0) /339.0
 plt.subplot()
 plot_test, = plt.plot(array[:1500], label='Normalized Load')
@@ -70,41 +37,41 @@ arrayX=np.array(listX)
 arrayy=np.array(listy)
 
 
-X['train']=arrayX[0:12000]
-X['test']=arrayX[12000:13000]
-X['val']=arrayX[13000:14000]
+X['train']=arrayX[0:13000]
+X['test']=arrayX[13000:14000]
 
-y['train']=arrayy[0:12000]
-y['test']=arrayy[12000:13000]
-y['val']=arrayy[13000:14000]
+y['train']=arrayy[0:13000]
+y['test']=arrayy[13000:14000]
 
+# Build the model
 
-# print y['test'][0]
-# print y2['test'][0]
+model = Sequential()
 
+model.add(LSTM(input_dim=1, output_dim=50, return_sequences=True))
 
-#X1, y2 = generate_data(np.sin, np.linspace(0, 100, 10000), TIMESTEPS, seperate=False)
-# create a lstm instance and validation monitor
-validation_monitor = learn.monitors.ValidationMonitor(X['val'], y['val'],
-                                                      every_n_steps=PRINT_STEPS,
-                                                      early_stopping_rounds=1000)
+model.add(Dropout(0.2))
 
-regressor.fit(X['train'], y['train'], monitors=[validation_monitor], logdir=LOG_DIR)
+model.add(LSTM(input_dim=100, output_dim=200, return_sequences=False))
+model.add(Dropout(0.2))
 
-predicted = regressor.predict(X['test'])
-rmse = np.sqrt(((predicted - y['test']) ** 2).mean(axis=0))
-score = mean_squared_error(predicted, y['test'])
-print ("MSE: %f" % score)
+model.add(Dense(output_dim=1))
+model.add(Activation("linear"))
 
-#plot_predicted, = plt.plot(array[:1000], label='predicted')
+model.compile(loss="mse", optimizer="rmsprop")
+
+# Fit the model to the data
+
+model.fit(X['train'], y['train'], batch_size=512, nb_epoch=10, validation_split=0.08)
+test_results = model.predict(X['test'])
+
+# Rescale the test dataset and predicted data
+
+test_results = test_results * 339 + 147
+y['test'] = y['test'] * 339 + 147
+
 
 plt.subplot()
-plot_predicted, = plt.plot(predicted, label='predicted')
+plot_predicted, = plt.plot(test_results, label='predicted')
 
-plot_test, = plt.plot(y['test'], label='test')
+plot_test, = plt.plot(y['test']  , label='test')
 plt.legend(handles=[plot_predicted, plot_test])
-
-
-
-
-
